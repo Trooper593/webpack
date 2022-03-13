5d59cbfaa2bec4f6157236b14e27632d14493661
Dmitry Kuznetsov <dimushka5.93@mail.ru>
2022-03-14 02:24:11 +0300

    optimization

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Post from '@/Post';
 import '@/styles/styles.css';
+import * as $ from 'jquery';
 
 const post = new Post('First post');
+
+$('pre').html(post.toString())
 
 console.log('Post to string:', post.toString());
