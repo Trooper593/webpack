4375ae33cf1c231c11bce207c6b71ccc301bd44f
Dmitry Kuznetsov <dimushka5.93@mail.ru>
2022-03-15 21:43:24 +0300

    how to write styles in modular css

--- src/components/hello/hello.js
+++ src/components/hello/hello.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './hello.module.css';
+import styles from './hello.module.css';
 
 export default function ({name = 'user'}){
-    return <div className="box">
-        <h2 className="alert">Hello, {name}</h2>
+    return <div className={styles.box}>
+        <h2 className={styles.alert}>Hello, {name}</h2>
         <p>Welcome to site!</p>
     </div>
 }
