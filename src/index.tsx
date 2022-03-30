b675abc00a31beba696f45bf562fa13152c5d812
Dmitry Kuznetsov <dimushka5.93@mail.ru>
2022-03-30 20:22:40 +0300

    added alias

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
-import { Hello } from './components/hello/hello';
+import { Hello } from '@/components/hello/hello';
 
 ReactDOM.render(
   <Hello />,
